9d114b6844faac6cd6262348220a7bc851b54c06
Subarna704 <146692039+Subarna704@users.noreply.github.com>
2024-08-18 14:46:20 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// document.querySelector('.btn').addEventListener("click", () => {
+//     let price  = Number(document.querySelector('.price').value);
+//     const msg = document.querySelector('.msg');
+//     console.log(typeof price);
+//     if ( price < 40) {
+//         msg.innerText = `Total price is $${price + 10} with $10 shipping cost`;
+//     }
+//     else{
+//         msg.innerText = `Total price is $${price } with $0 shipping cost`;
+//     }
+// })
+
