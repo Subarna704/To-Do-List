4347a5dcd3b96bf83d3daa0ae52db7c9dffcb08f
subarna704 <subarnashrestha704@gmail.com>
2024-08-25 13:03:06 +1000

    forEach loop

--- To-Do-List.js
+++ To-Do-List.js
@@ -11,21 +11,32 @@ const date = document.querySelector(".date");
 
 const showlist = () => {
     let code = "";
-    for(i=0; i<myArray.length; i++) {
-        const newArrayobject = myArray[i];
-        const {name} = newArrayobject;
-        const {duedate} = newArrayobject;
+
+    myArray.forEach((value,index)=> {
+        const {name} = value;
+        const {duedate} = value; 
+
+        console.log(name, duedate)
+    
+
+        // for(i=0; i<myArray.length; i++) {
+        //     const newArrayobject = myArray[i];
+        //     const {name} = newArrayobject;
+        //     const {duedate} = newArrayobject;
         
         code += `
         <div class="list-css">${name}</div>
         <div class="list-css">${duedate}</div>
-        <button class="delete-css"onclick="
-            myArray.splice(${i}, 1);
-            showlist();
-        ">Delete</button>
+        <button class="delete-css" >Delete</button>
         `;
-    }
+    })
     msg.innerHTML = code;
+
+    document.querySelectorAll(".delete-css").addEventListener("click", (index)=> {
+        myArray.splice(index, 1);
+        showlist();
+
+    })
   
 }
 
